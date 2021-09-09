f99f1f0b6a822cb2bfa4ae47c45fb8ab0f313fd4
ryanhyunlee <ryanjhlee87@outlook.com>
2021-09-09 12:41:14 +0900

    Add chart

--- src/components/featuredInfo/FeaturedInfo.js
+++ src/components/featuredInfo/FeaturedInfo.js
@@ -9,7 +9,7 @@ const FeaturedInfo = () => {
         <div className='featuredMoneyContainer'>
           <span className='featuredMoney'>$2,415</span>
           <span className='featuredMoneyRate'>
-            -11.4 <ArrowDownward />
+            -11.4 <ArrowDownward className='featuredIcon negative' />
           </span>
         </div>
         <span className='featuredSub'>Compared to last month</span>
@@ -20,7 +20,7 @@ const FeaturedInfo = () => {
         <div className='featuredMoneyContainer'>
           <span className='featuredMoney'>$4,415</span>
           <span className='featuredMoneyRate'>
-            -1.4 <ArrowDownward />
+            -1.4 <ArrowDownward className='featuredIcon negative' />
           </span>
         </div>
         <span className='featuredSub'>Compared to last month</span>
@@ -31,7 +31,7 @@ const FeaturedInfo = () => {
         <div className='featuredMoneyContainer'>
           <span className='featuredMoney'>$2,225</span>
           <span className='featuredMoneyRate'>
-            +2.4 <ArrowUpward />
+            +2.4 <ArrowUpward className='featuredIcon' />
           </span>
         </div>
         <span className='featuredSub'>Compared to last month</span>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,19 @@
 import "./Home.css";
+import { userData } from "../../data";
 
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
 
 const Home = () => {
   return (
     <div className='home'>
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title='User Analytics'
+        grid
+        dataKey='Active User'
+      />
     </div>
   );
 };
